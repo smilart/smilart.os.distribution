ba348609ac6c0ce5a3001794c83d67d4b9056903
nsebyakina <sebyakina.n.a@smilart.com>
2014-08-25 17:42:15 +0400

    Простой способ записи iso на usb

--- scripts/installer-build/image_to_usb.sh
+++ scripts/installer-build/image_to_usb.sh
@@ -1,32 +1,8 @@
 #!/bin/bash
 
-set -vx
-#apt-get install debootstrap syslinux squashfs-tools genisoimage memtest86+ rsync gcc
+SOURCE=$1 
+DEVICE=$2
 
+# image_to_usb ./debian-live-amd64.iso /dev/sdb
 
-SOURCE=$1 # Path to image-dir
-PASSWD=$2
-DEVICE=$3
-TMP_LOCAL_DIR=/tmp/to_usb
-
-syslinux -i "$DEVICE"1
-#dd if=/usr/lib/syslinux/mbr.bin of=$DEVICE conv=notrunc bs=440 count=1
-dd if=/usr/sahre/syslinux/mbr.bin of=$DEVICE conv=notrunc bs=440 count=1
-[[ ! -d /mnt/usb ]] && mkdir -pv /mnt/usb
-mount  "$DEVICE"1 /mnt/usb
-
-
-
-[[ -d $TMP_LOCAL_DIR ]] && rm -rv $TMP_LOCACL_DIR
-mkdir -pv $TMP_LOCAL_DIR
-#rsync -rv root@192.168.1.52:/home/smilart.os.distribution/scripts/installer-build/live_boot/image /tmp/111
-#sshpass -p "smilart" rsync -rv root@192.168.1.52:/home/smilart.os.distribution/scripts/installer-build/live_boot/image /tmp/111
-sshpass -p $PASSWD rsync -rv  $SOURCE $TMP_LOCAL_DIR
-
-cp $TMP_LOCAL_DIR/image/isolinux/menu.c32 /mnt/usb/ && 
-cp $TMP_LOCAL_DIR/image/isolinux/hdt.c32 /mnt/usb/ && 
-cp $TMP_LOCAL_DIR/image/live/memtest /mnt/usb/memtest && 
-cp $TMP_LOCAL_DIR/image/isolinux/isolinux.cfg /mnt/usb/syslinux.cfg && 
-rsync -rv $TMP_LOCAL_DIR/image/live /mnt/usb/
-
-umount /mnt/usb
+dd if=$SOURCE of=$DEVICE
