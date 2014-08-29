586b0c603c657b4fe84eff12056bf9cbc07ee0fe
root <root@debian-52.smilart.com>
2014-08-29 19:32:18 +0400

    Изменения под сборку на debian 8

--- scripts/installer-build/build_live_iso.sh
+++ scripts/installer-build/build_live_iso.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 set -vx
-#apt-get install debootstrap syslinux squashfs-tools genisoimage memtest86+ rsync gcc git-core
+#apt-get install debootstrap syslinux squashfs-tools genisoimage memtest86+ rsync gcc git-core isolinux live-boot
 if [ -z "$1" ]; then
     ARCH="amd64"
 else
@@ -26,7 +26,7 @@ cp -pv ../to_iso/inittab chroot/etc/inittab
 gcc ../to_iso/autologin.c -o chroot/usr/local/sbin/autologin
 mkdir -p chroot/var/lib/smilart_srv/scripts && cp -pv ../../smilartos-install/smilartos-install.py chroot/var/lib/smilart_srv/scripts/smilartos-install.py && cp -pv ../to_iso/config_network.sh chroot/var/lib/smilart_srv/scripts/config_network.sh
 cp -pv ../to_iso/smilartos-install chroot/etc/init.d/smilartos-install
-cp -pv ../to_iso/after_install_to_xen.sh chroot/var/lib/smilart_srv/scriptsafter_install_to_xen.sh
+cp -pv ../to_iso/after_install_to_xen.sh chroot/var/lib/smilart_srv/scripts/after_install_to_xen.sh
 
 wget https://pypi.python.org/packages/source/u/urwid/urwid-1.2.1.tar.gz -P chroot/tmp
 
@@ -49,10 +49,16 @@ cp chroot/boot/initrd.img-3.2.0-4-$ARCH image/live/initrd1
 
 cp ../to_iso/isolinux.cfg image/isolinux/isolinux.cfg
 
-cp /usr/lib/syslinux/isolinux.bin image/isolinux/ && 
-cp /usr/lib/syslinux/menu.c32 image/isolinux/ && 
-cp /usr/lib/syslinux/hdt.c32 image/isolinux/ && 
+#cp /usr/lib/syslinux/isolinux.bin image/isolinux/ && 
+#cp /usr/lib/syslinux/menu.c32 image/isolinux/ && 
+#cp /usr/lib/syslinux/hdt.c32 image/isolinux/ && 
+#cp /boot/memtest86+.bin image/live/memtest
+
+cp chroot/usr/lib/syslinux/isolinux.bin image/isolinux/ && 
+cp chroot/usr/lib/syslinux/menu.c32 image/isolinux/ && 
+cp chroot/usr/lib/syslinux/hdt.c32 image/isolinux/ && 
 cp /boot/memtest86+.bin image/live/memtest
+
 
 cd image && 
 genisoimage -rational-rock -volid "Debian Live" -cache-inodes -joliet -full-iso9660-filenames -b isolinux/isolinux.bin -c isolinux/boot.cat -no-emul-boot -boot-load-size 4 -boot-info-table -output ../debian-live-$ARCH.iso . && 
