2dde72350701a249292062ba14f8f997be2b8e80
Eugene Rummo <rummo.e.a@smilart.com>
2014-09-05 16:21:52 +0400

    Заполнение параметров в шаблоне cloud-config.

--- scripts/smilartos-install/smilartos-install.py
+++ scripts/smilartos-install/smilartos-install.py
@@ -9,11 +9,14 @@ def save(fileName, data):
 	f.close()
 
 class NetworkInfo:
-	ip = None
-	mask = None
-	gateway = None
-	dns = None
-	hostName = None
+	def __init__(self):
+		self.ip = None
+		self.mask = None
+		self.gateway = None
+		self.dns = None
+		self.hostName = None
+		self.name = "name network interface"
+		self.updateServer = "name update server"
 
 	def setIP(self, edit, text):
 		self.ip = text
@@ -33,31 +36,79 @@ class NetworkInfo:
 	def getAllSettings(self):
 		return ("--ip=%s" % self.ip  + " --mask=%s" % self.mask + " --gateway=%s" % self.gateway +
 			" --dns=%s" % self.dns)
+	def getPrefix(self):
+		prefix = sum([bin(int(x)).count('1') for x in self.mask.split('.')])
+		return prefix
 
-class InstallMenu:
+class CloudConfig:
+	def __init__(self, networkInfo, pathToCloudConfig):
+		self.hostname = "__hostname__"
+		self.name = "__name__" # network interface name
+		self.ip = "__ip__"
+		self.gateway = "__gateway__"
+		self.dns = "__dns__"
+		self.updateserver = "__updateserver__"
+		self.cloudConfig = pathToCloudConfig
+		self.networkInfo = networkInfo
 
-	pathToNodeInfo = "/var/lib/smilart_srv/utils/nodeinfo"
-	pathToConfigNetwork = "/var/lib/smilart_srv/scripts/config_network.sh"
-	titleMenu = u"Smilart Operation System"
-	firstMenuChoices = [u'Сбор данных для лицензии', u'Установка', u'Выход']
-	StartMenu = None
-	pathToLicenseFile = None
+	def makeCloudConfig(self):
+		templateFile = open(self.cloudConfig, 'r')
+		lines = templateFile.readlines()
+		templateFile.close()
 
-	palette = [
-		('reversed', 'standout', ''),
-		('body','black','light gray', 'standout'),
-		('reverse','light gray','black'),
-		('header','white','dark red', 'bold'),
-		('important','dark blue','light gray',('standout','underline')),
-		('editfc','white', 'dark blue', 'bold'),
-		('editbx','light gray', 'dark blue'),
-		('editcp','black','light gray', 'standout'),
-		('bright','dark gray','light gray', ('bold','standout')),
-		('buttn','black','dark cyan'),
-		('buttnf','white','dark blue','bold'),
-	]
+		file = open(self.cloudConfig, 'w')
+		lines = self.insertVariables(lines)
+		file.writelines(lines)
+		file.close()
 
+	def insertVariables(self, lines):
+		newLines= []
+		for line in lines:
+			if line.find(self.hostname) != -1:
+				line = line.replace(self.hostname, self.networkInfo.hostName)
+			elif line.find(self.name) != -1:
+				line = line.replace(self.name, self.networkInfo.name)
+			elif line.find(self.ip) != -1:
+				ip = self.networkInfo.ip + "/" + str(self.networkInfo.getPrefix())
+				line = line.replace(self.ip, ip)
+			elif line.find(self.gateway) != -1:
+				line = line.replace(self.gateway, self.networkInfo.gateway)
+			elif line.find(self.dns) != -1:
+				line = line.replace(self.dns, self.networkInfo.dns)
+			elif line.find(self.updateserver) != -1:
+				line = line.replace(self.updateserver, self.networkInfo.updateServer)
+			newLines.append(line)
+		return newLines
 
+class Installator:
+
+	def __init__(self):
+		self.pathToNodeInfo = "/var/lib/smilart_srv/utils/nodeinfo"
+		self.pathToConfigNetwork = "/var/lib/smilart_srv/scripts/config_network.sh"
+		self.pathToCoreosInstall = "/var/lib/smilart_srv/coreos/coreos-install"
+		self.pathToCloudConfig = "/var/lib/smilart_srv/scripts/cloud-config"
+		self.titleMenu = u"Smilart Operation System"
+		self.firstMenuChoices = [u'Сбор данных для лицензии', u'Установка', u'Выход']
+		self.StartMenu = None
+		self.pathToLicenseFile = None
+		self.networkInfo = NetworkInfo()
+
+		self.palette = [
+			('reversed', 'standout', ''),
+			('body','black','light gray', 'standout'),
+			('main shadow',  'dark gray',  'black'),
+        	('line',         'black',      'light gray', 'standout'),
+        	('bg background','light gray', 'black'),
+			('reverse','light gray','black'),
+			('header','white','dark red', 'bold'),
+			('important','dark blue','light gray',('standout','underline')),
+			('editfc','white', 'dark blue', 'bold'),
+			('editbx','light gray', 'dark blue'),
+			('editcp','black','light gray', 'standout'),
+			('bright','dark gray','light gray', ('bold','standout')),
+			('buttn','black','dark cyan'),
+			('buttnf','white','dark blue','bold'),
+		]
 
 	def firstMenu(self, choices):
 		body = [urwid.Text(self.titleMenu), urwid.Divider()]
@@ -78,7 +129,6 @@ class InstallMenu:
 
 	def getLicenseData(self):
 		info = self.getInfo()
-		info = "HDD_SERIAL MOTHERBOARD_SERIAL CPUID"
 		fileName = "/var/lib/smilart_srv/utils/result"
 		save(fileName, info)
 		body = [urwid.Text(self.titleMenu), urwid.Divider()]
@@ -89,12 +139,12 @@ class InstallMenu:
 		self.StartMenu.original_widget = urwid.ListBox(urwid.SimpleFocusListWalker(body))
 
 	def getInfo(self):
-		proc = subprocess.Popen(self.pathToNodeInfo, shell=True, stdout=subprocess.PIPE)
+		proc = subprocess.Popen(self.pathToNodeInfo, stdout=subprocess.PIPE)
 		out, err = proc.communicate()
-		if err:
-			e = ReturnedError(err)
-			e.output = out
-			raise e
+		code = proc.returncode
+		if code != 0:
+			print "Error"
+			raise urwid.ExitMainLoop()
 		return out
 
 	def getPathToLicenseFile(self, edit, text):
@@ -114,41 +164,38 @@ class InstallMenu:
 
 	def checkLicense(self, button):
 		# open License File
-		license = True
 		body = [urwid.Text(self.titleMenu), urwid.Divider()]
 		done = urwid.Button(u'Ok')
-		if license:
+		if os.path.exists(self.pathToLicenseFile):
 			body.append(urwid.Text(u'Файл лицензии установлен\n'))
 			urwid.connect_signal(done, 'click', self.networkSettings)
 		else:
-			body.append(urwid.Text(u'Файл лицензии не верный\n'))
+			body.append(urwid.Text(u'Файл лицензии не найден\n'))
 			urwid.connect_signal(done, 'click', self.exit_program)
 
 		body.append(urwid.AttrMap(done, None, focus_map='reversed'))
 		self.StartMenu.original_widget = urwid.ListBox(urwid.SimpleFocusListWalker(body))
 
 	def networkSettings(self, button):
-		networkInfo = NetworkInfo()
-
 		ip_edit = urwid.AttrWrap(urwid.Edit(('editcp', u"Input Ip: ")), '', 'reversed')
-		urwid.connect_signal(ip_edit.base_widget, 'change', networkInfo.setIP)
+		urwid.connect_signal(ip_edit.base_widget, 'change', self.networkInfo.setIP)
 
 		mask_edit = urwid.AttrWrap(urwid.Edit(('editcp', u"Input Mask: ")), '', 'reversed')
-		urwid.connect_signal(ip_edit.base_widget, 'change', networkInfo.setMask)
+		urwid.connect_signal(mask_edit.base_widget, 'change', self.networkInfo.setMask)
 
 		gateway_edit = urwid.AttrWrap(urwid.Edit(('editcp', u"Input Gateway: ")), '', 'reversed')
-		urwid.connect_signal(ip_edit.base_widget, 'change', networkInfo.setGateway)
+		urwid.connect_signal(gateway_edit.base_widget, 'change', self.networkInfo.setGateway)
 
 		dns_edit = urwid.AttrWrap(urwid.Edit(('editcp', u"Input DNS: ")),	'', 'reversed')
-		urwid.connect_signal(ip_edit.base_widget, 'change', networkInfo.setDNS)
+		urwid.connect_signal(dns_edit.base_widget, 'change', self.networkInfo.setDNS)
 
 		hostName_edit = urwid.AttrWrap(urwid.Edit(('editcp', u"Input Host name: ")), '', 'reversed')
-		urwid.connect_signal(ip_edit.base_widget, 'change', networkInfo.setHostName)
+		urwid.connect_signal(hostName_edit.base_widget, 'change', self.networkInfo.setHostName)
 
 		blank = urwid.Divider()
 		done = urwid.Button(u'Ok')
 
-		urwid.connect_signal(done, 'click', self.configNetwork, networkInfo)
+		urwid.connect_signal(done, 'click', self.configNetwork)
 
 		listbox_content = [
 			urwid.Text(self.titleMenu),
@@ -165,14 +212,77 @@ class InstallMenu:
 		listbox = urwid.ListBox(urwid.SimpleFocusListWalker(listbox_content))
 		self.StartMenu.original_widget = listbox
 
-	def configNetwork(self, button, networkInfo):
-		proc = subprocess.Popen([self.pathToConfigNetwork, networkInfo.getAllSettings()], shell=True, stdout=subprocess.PIPE)
+	def configNetwork(self, button):
+		command = self.pathToConfigNetwork + " " + self.networkInfo.getAllSettings()
+		proc = subprocess.Popen(command.split(), stdout=subprocess.PIPE)
 		out, err = proc.communicate()
-		if err:
-			e = ReturnedError(err)
-			e.output = out
-			raise e
-		self.exit_program(button)
+		code = proc.returncode
+		if code != 0:
+			body = [urwid.Text(self.titleMenu), urwid.Divider()]
+			text = u'Network config error!\n ' + out + '\n ' + err
+			done = urwid.Button(u'Выход')
+			urwid.connect_signal(done, 'click', self.exit_program)
+			body.append(urwid.Text(text))
+			body.append(urwid.Divider())
+			body.append(urwid.AttrMap(done, None, focus_map='reversed'))
+			self.StartMenu.original_widget = urwid.ListBox(urwid.SimpleFocusListWalker(body))
+		else:
+			body = [urwid.Text(self.titleMenu), urwid.Divider()]
+			text = u'Network configure success!\n ' + out
+			done = urwid.Button(u'Далее')
+			urwid.connect_signal(done, 'click', self.createCloudConfig)
+			body.append(urwid.Text(text))
+			body.append(urwid.Divider())
+			body.append(urwid.AttrMap(done, None, focus_map='reversed'))
+			self.StartMenu.original_widget = urwid.ListBox(urwid.SimpleFocusListWalker(body))
+
+
+	def createCloudConfig(self, button):
+		config = CloudConfig(self.networkInfo, self.pathToCloudConfig)
+		config.makeCloudConfig()
+
+		body = [urwid.Text(self.titleMenu), urwid.Divider()]
+		install = urwid.Button(u'Установка CoreOS')
+		urwid.connect_signal(install, 'click', self.installCoreOS)
+		done = urwid.Button(u'Выход')
+		urwid.connect_signal(done, 'click', self.exit_program)
+		body.append(urwid.AttrMap(install, None, focus_map='reversed'))
+		body.append(urwid.AttrMap(done, None, focus_map='reversed'))
+		self.StartMenu.original_widget = urwid.ListBox(urwid.SimpleFocusListWalker(body))
+
+
+	def installCoreOS(self, button):
+		command =  self.pathToCoreosInstall + " -d dba -C stable -c " + self.pathToCloudConfig
+		proc = subprocess.Popen(command.split(), stdout=subprocess.PIPE)
+		out, err = proc.communicate()
+		code = proc.returncode
+		if code != 0:
+			body = [urwid.Text(self.titleMenu), urwid.Divider()]
+			text = u"CoreOS didn't install!\n + " + out + "\n " + err
+			done = urwid.Button(u'Выход')
+			urwid.connect_signal(done, 'click', self.exit_program)
+			body.append(urwid.Text(text))
+			body.append(urwid.Divider())
+			body.append(urwid.AttrMap(done, None, focus_map='reversed'))
+			self.StartMenu.original_widget = urwid.ListBox(urwid.SimpleFocusListWalker(body))
+		else:
+			body = [urwid.Text(self.titleMenu), urwid.Divider()]
+			text = u'CoreOS installed successful'
+			copy = urwid.Button(u'Копировать файлы')
+			urwid.connect_signal(copy, 'click', self.copyServiceContainer)
+
+			done = urwid.Button(u'Выход')
+			urwid.connect_signal(done, 'click', self.exit_program())
+			body.append(urwid.Text(text))
+			body.append(urwid.Divider())
+			body.append(urwid.AttrMap(copy, None, focus_map='reversed'))
+			body.append(urwid.AttrMap(done, None, focus_map='reversed'))
+			self.StartMenu.original_widget = urwid.ListBox(urwid.SimpleFocusListWalker(body))
+
+
+	def copyServiceContainer(self, button):
+		pathFrom = ""
+		pathTo = ""
 
 
 	def exit_program(self, button):
@@ -188,5 +298,6 @@ class InstallMenu:
 		urwid.MainLoop(top, self.palette).run()
 
 
+
 if __name__ == '__main__':
-	InstallMenu().main()
+	Installator().main()
